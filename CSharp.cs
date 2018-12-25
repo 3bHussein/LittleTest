4ccbc89fbed1dca3d7a0fcd784e1737172fcaefe
3bHussein <39864308+3bHussein@users.noreply.github.com>
2018-12-25 10:18:13 +0200

    This is a console app
    it's make meeasge say hello :+1:

---
+++ CSharp.cs
@@ -0,0 +1,17 @@
+using System;
+
+
+
+
+namespace littletest
+{
+    public class people
+    {
+        static void Main(string[] arg)
+        {
+            Console.WriteLine("hello");
+            Console.ReadLine();
+        }
+    }
+}
+
